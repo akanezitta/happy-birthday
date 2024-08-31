f020188a6a089b2cda24d0e4e17f8302dfa4ce5e
akanezitta <147991973+akanezitta@users.noreply.github.com>
2024-08-30 22:09:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,16 +11,3 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 1000);
     });
 });
-document.getElementById('seu-botao').addEventListener('click', () => {
-    confetti({
-        particleCount: 100, // Quantidade de confetes
-        spread: 70, // Espalhamento dos confetes
-        origin: { y: 0.6 }, // Origem (altura) dos confetes
-    });
-});
-document.getElementById('seu-botao').addEventListener('click', () => {
-    const baloes = document.querySelectorAll('.balao');
-    baloes.forEach((balao) => {
-        // Adicione aqui a lógica para animar os balões (por exemplo, usando CSS transitions ou transformações)
-    });
-});
